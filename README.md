f138d92ffdd3d9a8f6de4d89c7589b24a2af797e
Tomasyhy <tomasz.hliwiadczyn@gmail.com>
2017-10-15 13:11:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+hproject
+========
+
+A Symfony project created on August 23, 2017, 8:09 am.
+# hproject
---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\Route;
+
+class ApiController extends FOSRestController
+{
+    /**
+     * @Route("/api")
+     */
+    public function indexAction()
+    {
+        $data = array("hello" => "world");
+        $view = $this->view($data);
+        return $this->handleView($view);
+    }
+}
